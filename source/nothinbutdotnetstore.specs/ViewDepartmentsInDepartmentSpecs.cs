884ea464df64ef60f2fed18ec726bbd89ddd310a
CodingInsanity <drone_evil@gmx.net>
2011-04-14 15:50:21 +0200

    Pushing new changes

--- source/nothinbutdotnetstore.specs/ViewDepartmentsInDepartmentSpecs.cs
+++ source/nothinbutdotnetstore.specs/ViewDepartmentsInDepartmentSpecs.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using developwithpassion.specifications.extensions;
 using developwithpassion.specifications.moq;
 using Machine.Specifications;
 using nothinbutdotnetstore.web.application;
@@ -16,7 +18,25 @@ namespace nothinbutdotnetstore.specs
         [Subject(typeof(ViewTheDepartmentsInADepartment))]
         public class when_run : concern
         {
-            It first_observation = () => 
+            private Establish e = () =>
+                                      {
+                                          currentDepartment = new DepartmentModel();
+                                          request_details = fake.an<IContainRequestDetails>();
+                                          response_engine = depends.on<ICanDisplayReportModels>();
+                                          reporting_gateway = depends.on<ICanFindInformationInTheStoreCatalog>();
+                                          reporting_gateway.setup(x => x.get_child_departments).Return(childDepartments);
+
+
+                                      };
+
+            private It should_tell_the_display_engine_to_display_the_departments_of_the_given_department = () => response_engine.received(x => x.display(childDepartments));
+
+            static ICanDisplayReportModels response_engine;
+            static IContainRequestDetails request_details;
+            static ICanFindInformationInTheStoreCatalog reporting_gateway;
+            
+            private static IEnumerable<DepartmentModel> childDepartments;
+            private static DepartmentModel currentDepartment;
         }
     }
 }
